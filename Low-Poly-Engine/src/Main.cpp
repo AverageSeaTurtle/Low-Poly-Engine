652028e535a5e2464386c48e5b286a86df9dcd9b
Armando Serrado de Almeida <71193715+ArmandoSerrado@users.noreply.github.com>
2022-06-03 20:19:17 -0300

    Removed visual-studio files

--- Low-Poly-Engine/src/Main.cpp
+++ Low-Poly-Engine/src/Main.cpp
@@ -1,6 +1,7 @@
 #include "Application.h"
 
 #include "Vector3D.h"
+#include "Node.h"
 
 #include <iostream>
 
@@ -11,7 +12,16 @@ int main(void)
     if (!App.Init())
         return -1;
 
-    //App.Loop();
+    Node A, B;
+    A.position = Vector3D(1, 1, 1);
+    B.position = Vector3D(10, 0, 3);
+
+    A.AddChildNode(B);
+    A.Update();
+
+    std::cout << B.GetParentNode().position << std::endl;
+
+    App.Loop();
 
     return 0;
 }
